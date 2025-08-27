3ea2713d78161ed78abc0a3486f558c66d0039aa
obadakatsha-ayatgroup <obada.Katsha@ayat-group.com>
2025-08-27 14:36:47 +0300

    update package.json and vite.config.ts files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,4 +9,12 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    port: 5173,
+    host: '0.0.0.0',
+    open: true
+  },
+  define: {
+    global: 'globalThis',
+  }
 });
